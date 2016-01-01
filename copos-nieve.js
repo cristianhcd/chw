684e7f30fd7b4ab340ee8c3f6eedf8db3ffff6a9
cristianhcd <cristianhcd@gmail.com>
2015-12-31 23:37:12 -0300

    Create copos-nieve.js

---
+++ copos-nieve.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="https://cloud.github.com/downloads/kopipejst/jqSnow/jquery.snow.min.1.0.js"></script>
+
+<script>
+$(document).ready( function(){
+ $.fn.snow();
+});
+</script>
